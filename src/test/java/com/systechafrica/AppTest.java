18ba97b583975d1e704aba236a3b0f04f722b7a9
IsabelNzioka <isabelnzioka@gmail.com>
2023-10-23 05:25:04 +0300

    Configure java with maven

---
+++ src/test/java/com/systechafrica/AppTest.java
@@ -0,0 +1,25 @@
+package com.systechafrica;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)  //? default instance
+public class AppTest 
+{
+
+
+    @Test
+    void add() {
+        // when
+        int result = 2 + 3;
+        // then
+        int expected = 5;
+        // verify thst result = expected
+        Assertions.assertEquals(expected, result, " The sum of 2 + 3 should be 5");
+
+    }
+
+   
+ 
+}
